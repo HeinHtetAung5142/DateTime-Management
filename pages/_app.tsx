d7e7576d287b2ec0fbf89e9922afb56dd3c3c952
Hein Htet Aung <hhaung5142@gmail.com>
2023-03-06 03:13:59 +0800

    Ported over code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,5 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { CronJob } = require("cron");
-  const fetch = require("node-fetch");
-
-  new CronJob(
-    "* * * * * *",
-    async function () {
-      console.log("GETTING!");
-      const response = await fetch("http://localhost:3000/api/hello");
-      console.log("RESPONSE", response);
-    },
-    null,
-    true,
-    "America/Los_Angeles"
-  );
-
   return <Component {...pageProps} />;
 }
